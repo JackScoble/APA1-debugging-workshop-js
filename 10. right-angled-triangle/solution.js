0e6acc00f7429f9f34083802aa6332861772cd8d
JackScoble <jack.scoble@ada.ac.uk>
2024-12-09 13:40:03 +0000

    Altered 10. right-angled-triangle to use * instead of # and repeat the correct number of times

--- 10. right-angled-triangle/solution.js
+++ 10. right-angled-triangle/solution.js
@@ -1,8 +1,6 @@
 function drawRightAngledTriangle(n) {
-  let i = 0;
-  while (i < n) {
-    console.log("#".repeat(i));
-    i++;
+  for(let i = 1; i <= n; i++) {
+    console.log("*".repeat(i));
   }
 }
 
