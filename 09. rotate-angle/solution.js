ec5808bd714d1ca53e3af3a2d09c20bec146100f
JackScoble <jack.scoble@ada.ac.uk>
2024-12-09 13:34:58 +0000

    Altered 09. rotate-angle to include edge cases and situations where the result angle is either above or below 360

--- 09. rotate-angle/solution.js
+++ 09. rotate-angle/solution.js
@@ -1,8 +1,15 @@
 function rotateAngleByDegrees(initialAngle, rotation) {
   // Calculate the new angle
   let newAngle = initialAngle + (rotation % 360);
-
-  return newAngle;
+  let finalAngle;
+  if(newAngle > 360) {
+    finalAngle = newAngle - 360; 
+  } else if (newAngle < 0) {
+    finalAngle = newAngle + 360;
+  } else {
+    finalAngle = newAngle;
+  }
+  return finalAngle === 360 ? 0:finalAngle;
 }
 
 module.exports = { rotateAngleByDegrees };
